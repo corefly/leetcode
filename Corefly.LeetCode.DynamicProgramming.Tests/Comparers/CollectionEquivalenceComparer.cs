5436bc3ba60de040ce4fa08ddb85bc63e6ca05f2
Andrey Zhuk <jukandrey208@gmail.com>
2022-07-04 19:32:57 +0300

    338. Counting Bits

---
+++ Corefly.LeetCode.DynamicProgramming.Tests/Comparers/CollectionEquivalenceComparer.cs
@@ -0,0 +1,33 @@
+﻿namespace Corefly.LeetCode.DynamicProgramming.Tests.Comparers;
+
+internal class CollectionEquivalenceComparer<T> : IEqualityComparer<IEnumerable<T>> where T : IEquatable<T>
+{
+    public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+    {
+        var leftList = new List<T>(x);
+        var rightList = new List<T>(y);
+
+        leftList.Sort();
+        rightList.Sort();
+
+        using var enumeratorX = leftList.GetEnumerator();
+        using var enumeratorY = rightList.GetEnumerator();
+
+        while (true)
+        {
+            var hasNextX = enumeratorX.MoveNext();
+            var hasNextY = enumeratorY.MoveNext();
+
+            if (!hasNextX || !hasNextY)
+                return (hasNextX == hasNextY);
+
+            if (!enumeratorX.Current.Equals(enumeratorY.Current))
+                return false;
+        }
+    }
+
+    public int GetHashCode(IEnumerable<T> obj)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Corefly.LeetCode.DynamicProgramming.Tests/CountingBitsTest.cs
@@ -0,0 +1,24 @@
+using Corefly.LeetCode.DynamicProgramming.Tests.Comparers;
+
+namespace Corefly.LeetCode.DynamicProgramming.Tests;
+
+public class CountingBitsTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var expected = new[] { 0, 1, 1, 2, 1, 2 };
+        var actual = CountingBits.CountBits(5);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var expected = new[] { 0, 1, 1 };
+        var actual = CountingBits.CountBits(2);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+}
