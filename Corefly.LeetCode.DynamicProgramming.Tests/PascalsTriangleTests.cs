6bb2a6db7d24bbd8b55ddb1e895aeaf7a2489d83
Andrey Zhuk <jukandrey208@gmail.com>
2022-07-04 20:14:35 +0300

    118. Pascal's Triangle

---
+++ Corefly.LeetCode.DynamicProgramming.Tests/PascalsTriangleTests.cs
@@ -0,0 +1,31 @@
+﻿using Corefly.LeetCode.DynamicProgramming.Tests.Comparers;
+
+namespace Corefly.LeetCode.DynamicProgramming.Tests;
+
+public class PascalsTriangleTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var expected = new List<List<int>>
+        {
+            new() { 1 },
+            new() { 1,1 },
+            new() { 1,2,1 },
+            new() { 1,3,3,1 },
+            new() { 1,4,6,4,1 }
+        };
+        var actual = PascalsTriangle.Generate(5);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var expected = new List<List<int>> { new() { 1 } }; 
+        var actual = PascalsTriangle.Generate(1);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+}
---
+++ Corefly.LeetCode.DynamicProgramming/PascalsTriangle.cs
@@ -0,0 +1,38 @@
+﻿namespace Corefly.LeetCode.DynamicProgramming;
+
+public class PascalsTriangle
+{
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        var pascalsTriangle = new List<IList<int>>(numRows);
+        var prevRow = default(IList<int>);
+
+        while (numRows > 0)
+        {
+            prevRow = GenerateNextRow(prevRow);
+            pascalsTriangle.Add(prevRow);
+            numRows--;
+        }
+
+        return pascalsTriangle;
+    }
+
+    private static IList<int> GenerateNextRow(IList<int>? prevRow)
+    {
+        prevRow ??= new List<int>();
+
+        var nextRow = new List<int>();
+        nextRow.Add(1);
+
+        if (prevRow.Count > 0)
+        {
+            for (var i = 0; i < prevRow.Count - 1; i++)
+            {
+                var value = prevRow[i] + prevRow[i + 1];
+                nextRow.Add(value);
+            }
+            nextRow.Add(1);
+        }
+        return nextRow;
+    }
+}
