e10512d60cccba58cb263a97e330e525d9d18444
Andrey Zhuk <jukandrey208@gmail.com>
2022-07-04 20:21:27 +0300

    119. Pascal's Triangle II

---
+++ Corefly.LeetCode.DynamicProgramming.Tests/PascalsTriangle2Tests.cs
@@ -0,0 +1,33 @@
+﻿using Corefly.LeetCode.DynamicProgramming.Tests.Comparers;
+
+namespace Corefly.LeetCode.DynamicProgramming.Tests;
+
+public class PascalsTriangle2Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        var expected = new List<int> { 1, 3, 3, 1 };
+        var actual = PascalsTriangle2.GetRow(3);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var expected = new List<int> { 1 };
+        var actual = PascalsTriangle2.GetRow(0);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var expected = new List<int> { 1, 1 };
+        var actual = PascalsTriangle2.GetRow(1);
+
+        Assert.Equal(expected, actual, new CollectionEquivalenceComparer<int>());
+    }
+}
---
+++ Corefly.LeetCode.DynamicProgramming/PascalsTriangle2.cs
@@ -0,0 +1,11 @@
+﻿namespace Corefly.LeetCode.DynamicProgramming;
+
+public class PascalsTriangle2
+{
+    public static IList<int> GetRow(int rowIndex)
+    {
+        var pascalsTriangle = PascalsTriangle.Generate(rowIndex + 1);
+
+        return pascalsTriangle[rowIndex];
+    }
+}
