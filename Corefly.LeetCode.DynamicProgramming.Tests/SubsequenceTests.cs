5dbe03a45ce2e384877f2174e162b48057490578
Andrey Zhuk <jukandrey208@gmail.com>
2022-07-05 19:25:07 +0300

    392. Is Subsequence

---
+++ Corefly.LeetCode.DynamicProgramming.Tests/SubsequenceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Corefly.LeetCode.DynamicProgramming.Tests;
+
+public class SubsequenceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(Subsequence.IsSubsequence("abc", "ahbgdc"));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.False(Subsequence.IsSubsequence("axc", "ahbgdc"));
+    }
+}
---
+++ Corefly.LeetCode.DynamicProgramming/Subsequence.cs
@@ -0,0 +1,16 @@
+﻿namespace Corefly.LeetCode.DynamicProgramming;
+
+public class Subsequence
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return true;
+        }
+
+        var i = 0;
+
+        return t.Any(ch => s[i] == ch && ++i == s.Length);
+    }
+}
