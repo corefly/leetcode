e68fa50d1ddb55fea814544f23bf674d38b4c186
Andrey Zhuk <jukandrey208@gmail.com>
2022-10-26 17:12:50 +0300

    Solve maximum subarray

---
+++ DynamicProgramming/MaximumSubarray.cs
@@ -0,0 +1,18 @@
+﻿namespace DynamicProgramming;
+
+public class MaximumSubarray
+{
+    public int MaxSubArray(int[] nums) {
+        var n = nums.Length;
+        var dp = new int[n];
+        dp[0] = nums[0];
+        var max = dp[0];
+        
+        for(var i = 1; i < n; i++){
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.Max(max, dp[i]);
+        }
+        
+        return max;
+    }
+}
