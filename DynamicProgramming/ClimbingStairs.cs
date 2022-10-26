c21d8680fd59b2da25506b6572214fad1a04270e
Andrey Zhuk <jukandrey208@gmail.com>
2022-10-26 16:52:14 +0300

    Solve climbing stairs

---
+++ DynamicProgramming/ClimbingStairs.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace DynamicProgramming;
+
+public class ClimbingStairs
+{
+    public static int ClimbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        var n1 = 1;
+        var n2 = 2;
+        for(var i = 3; i < n + 1; i++) {
+            var t = n1;
+            n1 = n2;
+            n2 = t + n2;
+        }
+        
+        return n2;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var expected = 2;
+        var actual = ClimbStairs(2);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var expected = 3;
+        var actual = ClimbStairs(3);
+
+        Assert.Equal(expected, actual);
+    }
+}
