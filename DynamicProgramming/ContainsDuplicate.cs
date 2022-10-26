4846bad3702a42495e3fb22b32026c52da558f9e
Andrey Zhuk <jukandrey208@gmail.com>
2022-10-26 17:08:35 +0300

    Solve contains duplicate

---
+++ DynamicProgramming/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿namespace DynamicProgramming;
+
+public class ContainsDuplicate
+{
+    public static bool IsContainsDuplicate(int[] nums)
+    {
+        var set = new HashSet<int>(nums.Length);
+
+        foreach (var num in nums)
+        {
+            var isAddedNum = set.Add(num);
+
+            if (!isAddedNum)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
